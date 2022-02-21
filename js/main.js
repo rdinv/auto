67a4149f5489c6d120f312a5960e562a3b56243e
rdinv <radone21@gmail.com>
2022-02-21 16:35:04 +0200

    scrollUp BTN add

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ jQuery(document).ready(function() {
 $(window).on("load", function() {
     document.body.classList.remove('no-scroll');
  });
+
+// scroll to top
+const showOnPx = 700;
+const backToTopButton = document.querySelector(".scroll_up")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: "smooth",
+    });
+  };
+backToTopButton.addEventListener("click", goToTop);
